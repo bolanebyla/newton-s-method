a5d95a69ffcadbbb87600537d1cb1280901f63a8
Алексей <alexleskov2000@gmail.com>
2021-04-18 09:00:58 +0800

    Определить минимум для заданной функции методом Ньютона с аналитическими производными

--- main.py
+++ main.py
@@ -88,6 +88,64 @@ def find_min():
         print(f'|{i[0]} - {i[1]}| = {i[2]}')
 
 
+def dif1_a(x):
+    """
+    Первая производная (аналитическая).
+    :param x:
+    :return:
+    """
+    y = (2 * x - 2) / math.sqrt(1 - (x - 1) ** 4)
+    return y
+
+
+def dif2_a(x):
+    """
+    Вторая производная (аналитическая).
+    :param x:
+    :return:
+    """
+    y = (2 * (1 + (2 * (x - 1) ** 4) / (1 - (x - 1) ** 4))) / math.sqrt(1 - (x - 1) * 4)
+    return y
+
+
+def Newton_a(x):
+    """
+    Метод Ньютона (с аналитическими производными).
+    :param x:
+    :return:
+    """
+    eps = 1e-14
+    x_error = []
+    x2 = x
+    for i in range(100):
+        x1 = x2
+        x2 = x1 - (dif1_a(x1) / dif2_a(x1))
+        x_error.append([x2, x1, abs(x2 - x1)])
+        if abs(x2 - x1) < eps:
+            break
+    if dif2_a(x2) > 0:
+        print('Точка минимума: x = ', end='')
+    elif dif2_a(x2) < 0:
+        print('Точка максимума: x = ', end='')
+    print(x2)
+    R_theor = x_error[-2][2]
+    print('Теоретическая ошибка: R_theor =', R_theor)
+    return x2, x_error
+
+
+def find_min_a():
+    """
+    Поиск точки минимума.
+    :return:
+    """
+    x, x_error = Newton(0.5)
+    x_0 = 1
+    R_real = abs(x_0 - x)
+    print('Реальная ошибка: R_real =', R_real)
+
+    print('\nСписок ошибок:')
+    for i in x_error: print(f'|{i[0]} - {i[1]}| = {i[2]}')
+
+
 if __name__ == '__main__':
-    show_func()
-    find_min()
+    find_min_a()
